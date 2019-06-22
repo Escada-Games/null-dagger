bfc3010c010413716d9790d178a6945afe886d09
Guilherme R. Ribeiro <keybpr@gmail.com>
2019-06-22 19:45:55 -0300

    Added first music and it fades out nicely

--- scenes/global.gd
+++ scenes/global.gd
@@ -3,8 +3,17 @@ var sceneChanger=preload("res://scenes/sceneChanger/sceneChanger.tscn")
 var growingDiamond=preload("res://scenes/growingDiamond/growingDiamond.tscn")
 var shrinkingDiamond=preload("res://scenes/shrinkingDiamond/shrinkingDiamond.tscn")
 var hasDagger=false
-func _ready():OS.window_size*=2
+var musicCalm=preload("res://scenes/musicCalm.tscn")
+func _ready():
+	var i=musicCalm.instance()
+	self.add_child(i)
+	OS.window_size*=2
 func changeScene(scene):
 	var i=sceneChanger.instance()
 	i.scene=scene
+	print(self.get_children()[0].name)
+	var j=scene.instance()
+	if j.name=="2":
+		$musicCalm.fadeOut()
+	j.free()
 	get_tree().get_root().add_child(i)
