9bcde0980fffc88db45c8ac7da871d4125da8714
Guilherme R. Ribeiro <keybpr@gmail.com>
2019-06-22 22:33:10 -0300

    Second music should loop correctly now

--- scenes/pauseManager/pauseManager.gd
+++ scenes/pauseManager/pauseManager.gd
@@ -7,5 +7,8 @@ func _physics_process(delta):
 		get_tree().paused= not get_tree().paused
 		if get_tree().paused:
 			self.visible=true
+			$twnLabelPos.interpolate_property($marginCtn/label,"rect_position",get_parent().get_parent().global_position,OS.window_size/2,0.4,Tween.TRANS_BACK,Tween.EASE_OUT)
+			$twnLabelPos.start()
 		else:
 			self.visible=false
+			
